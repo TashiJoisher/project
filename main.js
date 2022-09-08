29b1c55cb30696e417d570661f14521db02edc99
TashiJoisher <84910358+TashiJoisher@users.noreply.github.com>
2022-09-08 15:42:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+ function predload(){}
+
+ function setup()
+{
+    canvas=createCanvas(300,300);
+    canvas.center();
+}
+
+function draw(){}
+
+function take_snapshot(){
+save("FileteredME.png")
+}
